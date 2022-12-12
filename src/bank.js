2c0aaaf7459072d32ef3930aaa6ec0b3561d9c25
Mykhailo Skliarenko <painliader@gmail.com>
2022-12-12 20:05:39 +0000

    #4 - create and test deposite method of Bank class

--- src/bank.js
+++ src/bank.js
@@ -10,14 +10,40 @@ class Bank {
     return this.transactions;
   }
 
-  getAccount = (id) => {
-    return this.accounts.find((account) => account.getId() === id);
+  getAccount = (accountId) => {
+    const user = this.accounts.find((account) => account.getId() === accountId);
+    return user;
   }
 
   createAccount = () => {
-    const newAccount = new Account(this.accounts.length + 1);
+    const newAccount = new Account(this.createId());
     this.accounts.push(newAccount);
     return newAccount.getId();
+  }
+
+  showBalance = (accountId) => {
+    const account = this.getAccount(accountId);
+    return Boolean(account) ? account.getBalance() : "user does not exist" ;
+  }
+
+  deposite = (accountId, sum, date) => {
+    const account = this.getAccount(accountId);
+    if (Boolean(account) && sum > 0){
+      account.topUp(sum);
+      const newTransaction = {
+        account: accountId,
+        date: date,
+        type: 'deposite',
+        sum: sum
+      }
+      this.transactions.push(newTransaction);
+    } else return 'wrong input'
+  }
+
+  private
+
+  createId = () => {
+    return this.accounts.length + 1;
   }
 
 }
--- test/bank.test.js
+++ test/bank.test.js
@@ -16,5 +16,34 @@ describe ('Bank unit tests', ()=> {
     testBank.createAccount();
     const Account = testBank.getAccount(1);
     expect(Account.getId()).toBe(1);
-  }) 
+  })
+
+  it('showBalance for userId = 1 should return 0', () => {
+    const testBank = new Bank();
+    testBank.createAccount();
+    const result = testBank.showBalance(1);
+    expect(result).toBe(0);
+  })
+
+  it('showBalance for non-existent id should return "user does not exist"', () => {
+    const testBank = new Bank();
+    const result = testBank.showBalance(2);
+    expect(result).toBe("user does not exist");
+  })
+
+  it('showBalance after deposite 4000 should return 4000', () => {
+    const testBank = new Bank();
+    testBank.createAccount();
+    testBank.deposite(1,4000,'10-11-2012')
+    const result = testBank.showBalance(1);
+    expect(result).toEqual(4000);
+  })
+
+  it('showBalance after incorrect deposite should return 0', () => {
+    const testBank = new Bank();
+    testBank.createAccount();
+    testBank.deposite(1,'10-11-1111','10-11-2012')
+    const result = testBank.showBalance(1);
+    expect(result).toEqual(0);
+  })
 })
